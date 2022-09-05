2916cce1844af38aecaac5a7adc0837f72dafdf7
Oleevarh <112704071+Oleevarh@users.noreply.github.com>
2022-09-06 00:55:01 +0100

    Add files via upload

---
+++ Backend-Task/number4/index.js
@@ -0,0 +1,37 @@
+
+
+    var cars = ['Ford', 'benz', 'toyota', 'tesla', 'honda'];
+
+    
+
+    console.log(cars);
+    console.log(cars[3]);
+    cars[0] = 'kia';
+    cars.push('range rover')
+
+    console.log(cars);
+
+    for(var i=0; i<cars.length; i++ ){
+    console.log('this car is '+ cars[i])
+    }
+
+    var i = 0;
+     while(i<cars.length){
+     console.log('this car is '+ cars[i])
+       i++;
+     }
+
+
+   
+
+    var cars = {brand: 'Ford', model: '2015', color: ['red', 'black', 'white']}
+
+    console.log(cars)
+    
+
+
+
+
+
+
+    
